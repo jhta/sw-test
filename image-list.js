5ad404afccaad367eab15e5996eb9e4cb49ffef6
jhta <jeisonhs93@gmail.com>
2016-05-27 01:20:34 -0500

    Platzi test

--- image-list.js
+++ image-list.js
@@ -3,9 +3,9 @@ var Path = 'gallery/';
 var Gallery = { 'images' : [
       
   {
-    'name'  : 'Darth Vader',
-    'alt' : 'A Black Clad warrior lego toy',
-    'url': 'gallery/myLittleVader.jpg',
+    'name'  : 'Platzi',
+    'alt' : 'funciona esta mierda',
+    'url': 'gallery/platzi.png',
     'credit': '<a href="https://www.flickr.com/photos/legofenris/">legOfenris</a>, published under a <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">Attribution-NonCommercial-NoDerivs 2.0 Generic</a> license.'
   },
 
