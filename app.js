7a0362bcddde66fbb92d5a9f751cf9e383912fdb
jhta <jeisonhs93@gmail.com>
2016-05-27 09:49:42 -0500

    add logs

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 // register service worker
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
-    
+  navigator.serviceWorker
+  .register('/sw-test/sw.js', { scope: '/sw-test/' })
+  .then(function(reg) {
+    console.log("register complete", reg);
     if(reg.installing) {
-      console.log('Service worker installing');
+      console.log('Service worker installing :D');
     } else if(reg.waiting) {
-      console.log('Service worker installed');
+      console.log('Service worker installed :D');
     } else if(reg.active) {
-      console.log('Service worker active');
+      console.log('Service worker active :D');
     }
     
   }).catch(function(error) {
     // registration failed
     console.log('Registration failed with ' + error);
   });
-};
+} else {
+  console.log("the browser can't support sw. Should be a shit like safary or IE");
+}
 
 // function for loading each image via XHR
 
--- sw.js
+++ sw.js
@@ -1,32 +1,85 @@
+var files = [
+  '/sw-test/',
+  '/sw-test/index.html',
+  '/sw-test/style.css',
+  '/sw-test/app.js',
+  '/sw-test/image-list.js',
+  '/sw-test/star-wars-logo.jpg',
+  '/sw-test/gallery/bountyHunters.jpg',
+  '/sw-test/gallery/platzi.png',
+  '/sw-test/gallery/snowTroopers.jpg'
+];
+
+
 this.addEventListener('install', function(event) {
+  console.log("WORKER: install in progress", event);
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/platzi.png',
-        '/sw-test/gallery/snowTroopers.jpg'
-      ]);
+    // v1: version
+    caches
+    .open('v1')
+    .then(function(cache) { // API helps to caching responses
+      console.info("WORKER: install complete");
+      console.log("cache", cache);
+      return cache
+      .addAll(files);
     })
   );
 });
 
 this.addEventListener('fetch', function(event) {
+  console.log("fetching...", event);
   var response;
-  event.respondWith(caches.match(event.request).catch(function() {
-    return fetch(event.request);
+
+  if (event.request.method !== 'GET') {
+      console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
+      return;
+    }
+
+  event.respondWith(
+    caches
+    .match(event.request)
+    .catch(function() {
+      console.log("fetched request", fetch(event.request)) 
+      return fetch(event.request);
   }).then(function(r) {
+    console.log("fetch response", r);
     response = r;
-    caches.open('v1').then(function(cache) {
-      cache.put(event.request, response);
+    caches
+    .open('v1')
+    .then(function(cache) {
+      cache
+      .put(event.request, response);
     });
+    console.log("result response", response.clone());
     return response.clone();
-  }).catch(function() {
-    return caches.match('/sw-test/gallery/platzi.png');
   }));
+
 });
+
+
+// self.addEventListener("activate", function(event) {
+//   console.log('WORKER: activate event in progress.');
+
+//   event.waitUntil(
+//     caches
+//       .keys()
+//       .then(function (keys) {
+//         return Promise.all(
+//           keys
+//             .filter(function (key) {
+//               // Filter by keys that don't start with the latest version prefix.
+//               return !key.startsWith(version);
+//             })
+//             .map(function (key) {
+//               /* Return a promise that's fulfilled
+//                  when each outdated cache is deleted.
+//               */
+//               return caches.delete(key);
+//             })
+//         );
+//       })
+//       .then(function() {
+//         console.log('WORKER: activate completed.');
+//       })
+//   );
+// });
